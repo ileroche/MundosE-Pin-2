1d82b013d1a42ccf6496b883384b41f71619bc24
IleanaRoche <ileale.roche@gmail.com>
2024-11-13 17:53:11 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3"{
-    bucket                 = "s3grupo15"
+    bucket                 = "s3devops15"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
     dynamodb_table         = "terraformstatelock"
